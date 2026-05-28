2f720015b37cb7c3a4b654e00194fd103f9fe0dc
kebi <kanglu-email@qq.com>
2026-05-28 10:24:15 +0800

    feat: 更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 export default function App() {
+    const [count, setCount] = useState<number>(0)
     return (
         <div>
             <p>子应用1</p>
+            <p>计数: {count}</p>
+            <button onClick={() => setCount(count + 1)}>增加</button>
             <input type='text' placeholder='子应用1输入框' />
         </div>
     )
